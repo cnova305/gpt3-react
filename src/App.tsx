5a13408bf858fdc50057aadd9957c348df92c008
Christopher Juwao <njuwao@icloud.com>
2022-05-22 17:04:28 +0200

    Navbar Component Integraton

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,32 @@ import './App.css';
 
 // Importing Components
 import Navbar from './components/navbar/Navbar';
-import Article from './components/article/Article';
-import Feature from './components/feature/Feature';
 import Cta from './components/cta/Cta';
 import Brand from './components/brand/Brand';
+
+// Importing Containers
+import Header from './containers/header/Header';
+import Blog from './containers/blog/Blog';
+import Footer from './containers/footer/Footer';
+import Possibility from './containers/possibility/Possibility';
+import Features from './containers/features/Features';
+import WhatGPT3 from './containers/whatGPT3/WhatGPT3';
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Article />
-      <Feature />
-      <Cta />
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
       <Brand />
+      <WhatGPT3 />
+      <Features/>
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
     </div>
   );
 }
