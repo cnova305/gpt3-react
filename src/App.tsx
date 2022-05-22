f9c4555f8230a1e5f6819f3f150287ec8235e180
Christopher Juwao <njuwao@icloud.com>
2022-05-22 14:32:24 +0200

    Creating Project Scaffold

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+// Importing Components
+import Navbar from './components/navbar/Navbar';
+import Article from './components/article/Article';
+import Feature from './components/feature/Feature';
+import Cta from './components/cta/Cta';
+import Brand from './components/brand/Brand';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Article />
+      <Feature />
+      <Cta />
+      <Brand />
     </div>
   );
 }
