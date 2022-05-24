f970f1e065cbafbdc30819328b52e3f3cab8c8e1
Christopher Juwao <njuwao@icloud.com>
2022-05-24 11:09:42 +0200

    Blog and Footer Component Integration

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import './Article.css';
 
-const Article = () => {
+interface ArticleProps {
+  imgUrl: string;
+  date: string;
+  title: string;
+}
+
+const Article = ({
+  imgUrl = '',
+  date ='',
+  title = '',
+  ...props
+}:ArticleProps) => {
   return (
-    <div>Article</div>
+    <div className='gpt3__blog-container_article'>
+      <div className='gpt3__blog-container_article-image'>
+        <img src={imgUrl} alt='blog' />
+      </div>
+      <div className='gpt3__blog-container_article-content'>
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
   )
 }
 
